9763dc809787853359b31ee8b5942a35fd71c187
Paul Crooks <pgcrooks@gmail.com>
2026-03-14 21:24:57 +0000

    feat(config): Add basic viper config

--- cmd/scraper/scraper.go
+++ cmd/scraper/scraper.go
@@ -5,24 +5,47 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/spf13/viper"
+
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	scanner_int "github.com/pgcrooks/dspm-scanner/internal"
 )
 
 func main() {
-	fmt.Println("Running scraper")
+	log.Println("starting scraper")
 
-	client := newS3Client()
-
-	contents, err := scanner_int.ListS3Bucket(context.TODO(), client, "pgcrooks-dspm")
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
 	if err != nil {
-		log.Fatal(err)
+		panic(fmt.Errorf("fatal error config file: %w", err))
 	}
 
-	log.Println("first page results")
-	for _, object := range contents {
-		log.Printf("key=%s size=%d", object.Key, object.Size)
+	if viper.GetBool("aws.enabled") {
+		log.Println("aws enabled")
+
+		bucketName := viper.GetString("aws.bucket_name")
+
+		client := newS3Client()
+
+		contents, err := scanner_int.ListS3Bucket(context.TODO(), client, bucketName)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println("first page results")
+		for _, object := range contents {
+			log.Printf("key=%s size=%d", object.Key, object.Size)
+		}
+	} else {
+		log.Println("aws disabled")
+	}
+
+	if viper.GetBool("local.enabled") {
+		log.Println("local enabled")
+	} else {
+		log.Println("local disabled")
 	}
 }
 
