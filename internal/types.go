9c1dca259d14a55c0549cec470f8638845aed442
Paul Crooks <pgcrooks@gmail.com>
2026-03-23 21:51:03 +0000

    feat(config): Add config object

--- internal/types.go
+++ internal/types.go
@@ -4,3 +4,14 @@ type BucketObject struct {
 	Key  string
 	Size int64
 }
+
+type Config struct {
+	Aws struct {
+		Enabled    bool
+		BucketName string
+	}
+	Local struct {
+		Enabled bool
+		Path    string
+	}
+}
