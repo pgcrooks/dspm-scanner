476211e4b80a1a7a47b15eaa109b5b3596ba36c3
Paul Crooks <pgcrooks@gmail.com>
2026-03-29 14:08:31 +0100

    chore(types): Remove unhelpful types

--- internal/datastore.go
+++ internal/datastore.go
@@ -2,10 +2,39 @@ package scanner_int
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log/slog"
 	"time"
 )
+
+type DataStoreType int
+
+const (
+	LocalDB DataStoreType = iota
+	Memory
+)
+
+type DataStore struct {
+	Name string
+	Type DataStoreType
+}
+
+type DataStoreLocalDB struct {
+	DataStore
+	SqlDB *sql.DB
+}
+
+type DataStoreMemory struct {
+	DataStore
+	// Actual store lives here
+}
+
+type IDataStore interface {
+	GetName() string
+	Close()
+	Run(ctx context.Context, messageChan <-chan BucketObjectBatch)
+}
 
 type DataStoreAPI interface {
 	Close()
