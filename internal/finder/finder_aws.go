4f7c75c4f54f8221d7d0a995561891037ba9938d
Paul Crooks <pgcrooks@gmail.com>
2026-04-05 23:04:48 +0100

    feat(finder): Create finder service iface

--- internal/finder/finder_aws.go
+++ internal/finder/finder_aws.go
@@ -3,17 +3,84 @@ package finder
 import (
 	"context"
 	"fmt"
+	"log/slog"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
+
+type finderAWSS3 struct {
+	Finder
+	Client     *s3.Client
+	BucketName string
+}
+
+func newFinderAWSS3(ctx context.Context, bucketName string) (IFinder, error) {
+	// Load the Shared AWS Configuration (~/.aws/config)
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	return &finderAWSS3{
+		Finder: Finder{
+			Name: "aws_s3",
+		},
+		Client:     client,
+		BucketName: bucketName,
+	}, nil
+}
+
+func (f finderAWSS3) Run(ctx context.Context) {
+	slog.Info("running aws s3 finder", "obj", f)
+
+	run := true
+	for run {
+		select {
+		case <-ctx.Done():
+			slog.Info("stopping finder local", "obj", f)
+			run = false
+
+		default:
+			contents, err := listS3Bucket(ctx, f.Client, f.BucketName)
+			if err != nil {
+				slog.Error(err.Error())
+			} else {
+				slog.Info("first page results")
+				for _, object := range contents {
+					slog.Info("key=%s size=%d", object.Key, object.Size)
+				}
+			}
+		}
+	}
+}
 
 type S3API interface {
 	ListObjectsV2(ctx context.Context, params *s3.ListObjectsV2Input, optFns ...func(*s3.Options)) (*s3.ListObjectsV2Output, error)
 }
 
-func ListS3Bucket(ctx context.Context, api S3API, bucket_name string) (BucketObjectBatch, error) {
-	output, err := api.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+// 	client, err := newS3Client()
+// 	if err != nil {
+// 		slog.Error("unable to create AWS client", "err", err.Error())
+// 	} else {
+// 		contents, err := finder.ListS3Bucket(
+// 			context.TODO(), client, config.Scraper.Aws.BucketName,
+// 		)
+// 		if err != nil {
+// 			slog.Error(err.Error())
+// 		} else {
+// 			slog.Info("first page results")
+// 			for _, object := range contents {
+// 				slog.Info("key=%s size=%d", object.Key, object.Size)
+// 			}
+// 		}
+// 	}
+
+func listS3Bucket(ctx context.Context, api S3API, bucket_name string) (BucketObjectBatch, error) {
+	output, err := api.ListObjectsV2(ctx, &s3.ListObjectsV2Input{
 		Bucket: aws.String(bucket_name),
 	})
 	if err != nil {
--- internal/finder/finder_local.go
+++ internal/finder/finder_local.go
@@ -2,34 +2,63 @@ package finder
 
 import (
 	"context"
-	"fmt"
 	"log"
+	"log/slog"
 	"os"
+	"time"
 )
 
-func listLocalBucket(ctx context.Context, path string) (BucketObjectBatch, error) {
-	files, err := os.ReadDir(path)
-	if err != nil {
-		return nil, fmt.Errorf("can not list bucket. %v", err)
-	}
+type finderLocal struct {
+	Finder
+	Path string
+}
 
-	var contents BucketObjectBatch
-	for _, file := range files {
-		// Ignore directories
-		if !file.IsDir() {
-			var fileSize int64 = 0
-			fileInfo, err := os.Stat(path + file.Name())
-			if err != nil {
-				log.Printf("failed to get file stat for %s", file.Name())
+func newFinderLocal(path string) (IFinder, error) {
+	return &finderLocal{
+		Finder: Finder{
+			Name: "local",
+		},
+		Path: path,
+	}, nil
+}
+
+func (f finderLocal) Run(ctx context.Context) {
+	slog.Info("running finder local", "obj", f)
+
+	run := true
+	for run {
+		select {
+		case <-ctx.Done():
+			slog.Info("stopping finder local", "obj", f)
+			run = false
+
+		default:
+			files, err := os.ReadDir(f.Path)
+			if err == nil {
+				for _, file := range files {
+					// Ignore directories
+					if !file.IsDir() {
+						var fileSize int64 = 0
+						fileInfo, err := os.Stat(f.Path + file.Name())
+						if err != nil {
+							log.Printf("failed to get file stat for %s", file.Name())
+						} else {
+							fileSize = fileInfo.Size()
+						}
+						obj := BucketObject{
+							Key:  file.Name(),
+							Size: fileSize,
+						}
+						slog.Info("found metadata", "file", obj)
+						//todo write to channel
+					}
+				}
 			} else {
-				fileSize = fileInfo.Size()
+				slog.Warn("cannot read path", "err", err.Error())
 			}
-			obj := BucketObject{
-				Key:  file.Name(),
-				Size: fileSize,
-			}
-			contents = append(contents, obj)
 		}
+
+		time.Sleep(time.Second)
 	}
-	return contents, nil
+	slog.Info("stopped finder local", "obj", f)
 }
